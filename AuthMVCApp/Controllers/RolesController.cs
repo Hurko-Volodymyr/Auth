8735fc9618dfc349f7109002c4f7e46ca5604d1e
Hurko Volodymyr <pzkpmfv@gmail.com>
2024-10-13 22:39:29 +0300

    done

---
+++ AuthMVCApp/Controllers/RolesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AuthMVCApp.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("admin/")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            var users = _userManager.Users.Select(u => new SelectListItem
+            {
+                Value = u.Id,
+                Text = u.UserName
+            }).ToList();
+
+            ViewBag.Users = users;
+            return View(roles);
+        }
+
+
+        [HttpGet]
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                IdentityResult result = await _roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(name);
+        }
+
+        [Route("assign-role")]
+        public async Task<IActionResult> AssignRole(string userId, string roleName)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user != null)
+            {
+                await _userManager.AddToRoleAsync(user, roleName);
+            }
+            return RedirectToAction("Index", "Home"); 
+        }
+    }
+}
---
+++ AuthMVCApp/Data/SeedRoles.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthMVCApp.Data
+{
+    public static class SeedRoles
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                string[] roleNames = { "Admin", "User" };
+                foreach (var roleName in roleNames)
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(roleName);
+                    if (!roleExist)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(roleName));
+                    }
+                }
+
+                var adminUser = await userManager.FindByNameAsync("admin");
+                if (adminUser == null)
+                {
+                    adminUser = new IdentityUser
+                    {
+                        UserName = "admin",
+                        Email = "veronika.khimenko@icloud.com",
+                        EmailConfirmed = true
+                    };
+                    await userManager.CreateAsync(adminUser, "AdminPassword123!");
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+            }
+        }
+        
+    }
+}
